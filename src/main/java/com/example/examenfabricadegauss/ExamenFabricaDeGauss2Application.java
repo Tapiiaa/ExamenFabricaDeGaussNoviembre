3896190f35775479281f33fa7d4526156219ac26
Tapiiaa <ptapilob@myuax.com>
2024-11-11 12:06:02 +0100

    Initial commit

---
+++ src/main/java/com/example/examenfabricadegauss/ExamenFabricaDeGauss2Application.java
@@ -0,0 +1,13 @@
+package com.example.examenfabricadegauss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFabricaDeGauss2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenFabricaDeGauss2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examenfabricadegauss/ExamenFabricaDeGauss2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examenfabricadegauss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFabricaDeGauss2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
