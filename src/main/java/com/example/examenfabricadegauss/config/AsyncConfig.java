6dc56eb3013824f31f095e0888fbb7ba9874df61
paatriksirbu <psirb@myuax.com>
2024-11-13 21:28:02 +0100

    EnableAsync

--- src/main/java/com/example/examenfabricadegauss/config/AsyncConfig.java
+++ src/main/java/com/example/examenfabricadegauss/config/AsyncConfig.java
@@ -2,9 +2,11 @@ package com.example.examenfabricadegauss.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig {
 
     @Bean(name = "customTaskExecutor")
