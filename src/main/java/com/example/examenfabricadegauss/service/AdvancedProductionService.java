2b7c1433e6ef92b9cf7a793809f66d893b808e92
paatriksirbu <psirb@myuax.com>
2024-11-14 00:02:42 +0100

    Mecanismo para agregar validacion de solicitudes de produccion. Garantiza que solo se programen taraes correctas.

--- src/main/java/com/example/examenfabricadegauss/service/AdvancedProductionService.java
+++ src/main/java/com/example/examenfabricadegauss/service/AdvancedProductionService.java
@@ -15,6 +15,11 @@ public class AdvancedProductionService {
     }
 
     public void handleProductionRequest(String componentType, int priority) {
+        if (componentType == null || componentType.isEmpty()) {
+            throw new IllegalArgumentException("El tipo de componente no puede ser nulo o vacío");
+        } else if (priority < 1 || priority > 10) {
+            throw new IllegalArgumentException("La prioridad debe estar entre 1 y 10");
+        }
         scheduler.scheduleTask(new ScheduledTask(componentType, priority));
     }
 }
