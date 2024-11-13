c684fc2508e76cf902c7e5e27d1c50709082df46
paatriksirbu <psirb@myuax.com>
2024-11-13 21:27:15 +0100

    Cambio de print por un logger.

--- src/main/java/com/example/examenfabricadegauss/service/AssemblyService.java
+++ src/main/java/com/example/examenfabricadegauss/service/AssemblyService.java
@@ -1,15 +1,20 @@
 package com.example.examenfabricadegauss.service;
 
-import com.example.examenfabricadegauss.config.RabbitConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+
+import com.example.examenfabricadegauss.config.RabbitConfig;
 
 @Service
 public class AssemblyService {
 
+    private static final Logger logger = LoggerFactory.getLogger(AssemblyService.class);
+
     @RabbitListener(queues = RabbitConfig.ASSEMBLY_QUEUE_NAME)
     public void receiveComponent(String componentType) {
-        System.out.println("Ensamblaje de componente: " + componentType);
+        logger.info("Ensamblaje de componente: {}", componentType); 
         // Añadir aquí la lógica de ensamblaje
     }
 }
