aa3a1d3b2e9f3717e33ea30061e69241bb73a608
Tapiiaa <ptapilob@myuax.com>
2024-11-13 15:35:54 +0100

    Completamos de momento service

--- src/main/java/com/example/examenfabricadegauss/service/ProductionService.java
+++ src/main/java/com/example/examenfabricadegauss/service/ProductionService.java
@@ -8,6 +8,10 @@ public class ProductionService {
     public void receiveMessage(String message) {
         System.out.println("Received <" + message + ">");
         // Procesar el mensaje recibido, transformarlo en componentes, etc.
+    }
 
+    //Patrik, completar si ves necesario:
+    private void processMessage(String message) {
+        System.out.println("Processing message: " + message); //Esto como me comentaste, procesamos la señal como msj.
     }
 }
