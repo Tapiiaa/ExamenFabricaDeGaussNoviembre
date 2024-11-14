7d4a9885ca84d50d69a8e48758836f6b78adb597
Patrik <psirb@myuax.com>
2024-11-14 11:20:26 +0100

    Threadpooltaskexecutor, constructor

--- src/main/java/com/example/examenfabricadegauss/util/ProductionScheduler.java
+++ src/main/java/com/example/examenfabricadegauss/util/ProductionScheduler.java
@@ -1,12 +1,24 @@
 package com.example.examenfabricadegauss.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.PriorityBlockingQueue;
 
 @Component
 public class ProductionScheduler {
+    private static final Logger logger = LoggerFactory.getLogger(ProductionScheduler.class);
+
     private final PriorityBlockingQueue<ScheduledTask> queue = new PriorityBlockingQueue<>();
+    private final ThreadPoolTaskExecutor taskExecutor;
+
+    @Autowired
+    public ProductionScheduler(ThreadPoolTaskExecutor taskExecutor) {
+        this.taskExecutor = taskExecutor;
+    }
 
     public void scheduleTask(ScheduledTask task) {
         queue.put(task);
