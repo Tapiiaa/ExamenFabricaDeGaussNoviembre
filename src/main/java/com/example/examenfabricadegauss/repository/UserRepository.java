125b342a9ac20efabd8b498635f904142f8f614c
Patrik <psirb@myuax.com>
2024-11-14 01:00:20 +0100

    UserRepository es una interfaz que proporciona CRUD automaticamente y podemos buscar al usuario mediante username o email.

--- src/main/java/com/example/examenfabricadegauss/repository/UserRepository.java
+++ src/main/java/com/example/examenfabricadegauss/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.example.examenfabricadegauss.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByEmail(String email);
+
 }
