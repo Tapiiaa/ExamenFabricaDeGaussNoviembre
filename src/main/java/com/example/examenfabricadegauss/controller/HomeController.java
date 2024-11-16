485a0d964647765d9b027c6a51a9202e1299fc44
paatriksirbu <psirb@myuax.com>
2024-11-16 16:44:09 +0100

    Arreglo en la ruta al html.

--- src/main/java/com/example/examenfabricadegauss/controller/HomeController.java
+++ src/main/java/com/example/examenfabricadegauss/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/home")
     public String home() {
-        return "index.html";
+        return "src/main/resources/static/index.html";
     }
 }
 
