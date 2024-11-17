0a9c2bfccaec79cbfd62b504b833fad8dcbcdba1
paatriksirbu <psirb@myuax.com>
2024-11-17 22:08:30 +0100

    Pequeño ajuste en asynconfig

--- src/main/java/com/example/examenfabricadegauss/config/AsyncConfig.java
+++ src/main/java/com/example/examenfabricadegauss/config/AsyncConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 @Configuration
 @EnableAsync
 public class AsyncConfig {
@@ -12,10 +14,11 @@ public class AsyncConfig {
     @Bean(name = "customTaskExecutor")
     public ThreadPoolTaskExecutor threadPoolTaskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);
-        executor.setMaxPoolSize(10);
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
         executor.setQueueCapacity(50);
         executor.setThreadNamePrefix("CustomExecutor-");
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         executor.initialize();
         return executor;
     }
